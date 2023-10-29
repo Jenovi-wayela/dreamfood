4e2c4cbaa3189507c4b2566ac87c40d3c16331ff
Jenovi Wayela <wjenovi@gmail.com>
2023-10-30 00:49:27 +0200

    contact page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home ]
+
+  def about
+    # Your code for the About page
+  end
+
+  def contact
+    # Your code for the Contact page
+  end
 
   def home
+    # Your code for the Home page
+  end
+
+  def gallery
+    # Your code for the Gallery page
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+
+  root 'pages#home'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'contact', to: 'pages#contact', as: 'contact'
+  get 'gallery', to: 'pages#gallery', as: 'gallery'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
