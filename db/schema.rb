db881ed27bd4a45544175ef881f87c8916b3232e
Jenovi Wayela <wjenovi@gmail.com>
2023-12-03 16:30:28 +0200

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -93,7 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_165422) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
